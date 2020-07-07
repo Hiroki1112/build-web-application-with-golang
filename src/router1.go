1fc52a83ede98f184303cd277560059349bf0fb8
Hiroki1112 <pystdy@yahoo.co.jp>
2020-07-07 09:30:53 +0900

    4-1章

---
+++ src/router1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyMux struct {
+}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhelloName(w, r)
+		return
+	}
+	if r.URL.Path == "/test"{
+		saypath(w,r)
+		return 
+	}
+	http.NotFound(w, r)
+	return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello myroute!")
+}
+
+func saypath(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,"Hello test!")
+}
+
+func main() {
+	mux := &MyMux{}
+	http.ListenAndServe(":9090", mux)
+}
